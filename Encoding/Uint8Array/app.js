9e056707ef5a0b1b55b67c4cf22d0122d999a2b9
Mohfazam <mohd.sarwar.code@gmail.com>
2024-11-19 01:29:19 +0530

    Integer to HExadecimal

--- Encoding/Uint8Array/app.js
+++ Encoding/Uint8Array/app.js
@@ -1,3 +1,9 @@
-const str = "Hello World";
-const binaryRepresentation = new TextEncoder().encode(str);
-console.log(binaryRepresentation);
+// const str = "Hello World";
+// const binaryRepresentation = new TextEncoder().encode(str);
+// console.log(binaryRepresentation);
+
+let y = 12;
+// y.toString();
+// console.log(y);
+y.toString(16);
+console.log(y.toString(16)); // 12 in hex is C
