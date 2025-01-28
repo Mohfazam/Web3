a4e1993cc0877f4b900d0897e9d9af2f13bf16cb
Mohfazam <mohd.sarwar.code@gmail.com>
2025-01-29 02:18:04 +0530

    Public Priavet Key

---
+++ public-private-key/index.ts
@@ -0,0 +1,15 @@
+import * as ed from "@noble/ed25519";
+
+async function main(){
+    const privKey = ed.utils.randomPrivateKey();
+
+    const message = new TextEncoder().encode("Hello Hyderabad");
+
+    const pubKey = await ed.getPublicKeyAsync(privKey);
+
+    const signature = await ed.signAsync(message, privKey);
+
+    const isValid = await ed.verifyAsync(signature, message, pubKey);
+
+    console.log(isValid);
+}
