13dd582158411f4411ac8026f56a7a156b08d099
Sarwar-Ziegler <zaswd-9147@ziegler-aerospace.co.uk>
2025-02-11 14:42:31 +0530

    Public Private Key Pair

---
+++ PublicPrivate-key/src/index.ts
@@ -0,0 +1,16 @@
+import * as ed from "@noble/ed25519";
+
+async function main(){
+    const privateKey = ed.utils.randomPrivateKey();
+    console.log("Private key:", privateKey);
+
+    const message = new TextEncoder().encode("Hello, World");
+
+    const pubKey = await ed.getPublicKey(privateKey);
+
+    const signature = await ed.signAsync(message, privateKey);
+
+    const isValid = await ed.verifyAsync(signature, message, pubKey);
+}
+
+main();
