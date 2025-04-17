ac0b42680eb961cc6619e73dcbfc3701db493afe
Mohfazam <mohd.sarwar.code@gmail.com>
2025-04-17 20:25:26 +0530

    Public private 3

--- Public_Private_Keys/src/first.ts
+++ Public_Private_Keys/src/first.ts
@@ -1,1 +1,22 @@
-console.log("hello");
+import * as ed from "@noble/ed25519"
+
+async function main(){
+    try{
+        const privateKey = ed.utils.randomPrivateKey();
+
+    const message = new TextEncoder().encode("Hello BOts");
+
+    const publicKey = await ed.getPublicKeyAsync(privateKey);
+
+    const signature = await ed.signAsync(message, privateKey);
+
+    const isValid = await ed.verifyAsync(signature, message, publicKey);
+
+    console.log(isValid);
+    } catch(e){
+        console.log("Something wrong: " + e);
+    }
+}
+
+
+main();
