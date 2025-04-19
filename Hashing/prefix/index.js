e4bdf85e16be1643e1cb3293e55c3f5a70e39a71
Mohfazam <mohd.sarwar.code@gmail.com>
2025-04-19 05:31:39 +0530

    Encoding

--- Hashing/prefix/index.js
+++ Hashing/prefix/index.js
@@ -18,3 +18,8 @@ function StartWithPrefix(prefix){
 
 const result  = StartWithPrefix('00000');
 console.log(`Input: ${result.input} \n hash: ${result.hash}`)
+
+
+
+
+// ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDHl0fDHwmlt7tLC8ADiYrGktaWfQYJ3dzp1PvsNIXi/RQl8icY6ZBJiU58nuZF2pKSMYxVdsb8/tU/nFeMlUumwTg8CjNNZNA6TMQ0fuOdE44H+8o5kxSje4wLI40KU6NGNN3492mdJ18ILdWLmGtAX/HVDtM9vUHTekFoJs21yw75Y4ce/NwBnx1btarkOXZl89RiT9cwKCdAkOlDm4+kTdn94NJ3ugD9EseFxTgQlTpEWdxTba0Wtc+CvYenidemUtMhmm3rHX5PZWBYyjlNzNc62AhtA4CuRjzBtwBLJBgMuWR65WH8qkvQg977PhdsKaikSsTxtcz0IVy10haEToOHbzm0HL11G2zj6XcdP8zKPdRhtH86/+X2nDBz4ZEWDgDu7miuBg9M1jMvy3391IXECT+55dffjW0xhnmLRQ1pGLwd0hFc3oXYxxMMCgO47Fs/K6VC3xFPE/60pDptMP4iUaBVeemmQLFW7NKaM6UzKz1gHPBuNJvWHF0QkU0= root@LAPTOP-KS8MIDP7
