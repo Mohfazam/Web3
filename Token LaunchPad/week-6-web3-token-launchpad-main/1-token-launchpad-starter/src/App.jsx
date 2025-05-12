ff2aab6c5b248202b6c77d260bbf459262fa5d2f
Mohfazam <mohd.sarwar.code@gmail.com>
2025-05-12 15:12:16 +0530

    Connecttion Provider

--- Token LaunchPad/week-6-web3-token-launchpad-main/1-token-launchpad-starter/src/App.jsx
+++ Token LaunchPad/week-6-web3-token-launchpad-main/1-token-launchpad-starter/src/App.jsx
@@ -1,9 +1,24 @@
+// wallet adapter imports
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import {
+    WalletModalProvider,
+    WalletDisconnectButton,
+    WalletMultiButton
+} from '@solana/wallet-adapter-react-ui';
+import '@solana/wallet-adapter-react-ui/styles.css';
+
+
+
 import './App.css'
 import { TokenLaunchpad } from './components/TokenLaunchpad'
 
 function App() {
   return (
-    <TokenLaunchpad></TokenLaunchpad>
+    <div>
+      <ConnectionProvider endpoint={"https://api.devnet.solana.com"}>
+      
+      </ConnectionProvider>
+    </div>
   )
 }
 
