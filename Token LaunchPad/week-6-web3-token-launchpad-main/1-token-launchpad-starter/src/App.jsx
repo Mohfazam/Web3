c5c5e00d2b500aa6832dbac687e4a18cc35bbe66
Mohfazam <mohd.sarwar.code@gmail.com>
2025-05-13 14:55:20 +0530

    Launchpad

--- Token LaunchPad/week-6-web3-token-launchpad-main/1-token-launchpad-starter/src/App.jsx
+++ Token LaunchPad/week-6-web3-token-launchpad-main/1-token-launchpad-starter/src/App.jsx
@@ -16,7 +16,19 @@ function App() {
   return (
     <div>
       <ConnectionProvider endpoint={"https://api.devnet.solana.com"}>
-      
+      <WalletProvider wallets={[]} autoConnect>
+        <WalletModalProvider>
+          <div style={{
+            display: "flex",
+            justifyContent:'space-between',
+            padding: 20
+          }}>
+            <WalletMultiButton />
+            <WalletDisconnectButton />  
+          </div>
+          <TokenLaunchpad />
+        </WalletModalProvider>
+      </WalletProvider>
       </ConnectionProvider>
     </div>
   )
