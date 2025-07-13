c20142bfdc8534493ffa1e2dd153dbf76b5ccc43
Mohfazam <mohd.sarwar.code@gmail.com>
2025-07-13 19:41:16 +0530

    <RequestAirdrop />

--- DAPP/src/Components/RequestAirdrop.tsx
+++ DAPP/src/Components/RequestAirdrop.tsx
@@ -11,8 +11,7 @@ export const RequestAirdrop = () => {
     return (
         <div className="flex flex-col">
                 <input className="border-8 p-4 m-4" type="text" placeholder="Amount.." />
-            <button onClick={requestAirdrop} className="border-4 p-2 bg-amber-300 hover:cursor-pointer">Request AirDrop</button>
-
+            <button onClick={requestAirdrop} className="border-4 p-2 mb-4 bg-amber-300 hover:cursor-pointer">Request AirDrop</button>
             <strong>{wallet.publicKey?.toBase58()}</strong>
         </div>
     )
