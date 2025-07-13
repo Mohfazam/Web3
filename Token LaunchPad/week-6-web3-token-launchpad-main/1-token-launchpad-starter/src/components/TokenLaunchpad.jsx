7df2113b2da8deeef516120c5a7a3ec883e4a6a0
Mohfazam <mohd.sarwar.code@gmail.com>
2025-07-13 15:54:05 +0530

    DAPP

--- Token LaunchPad/week-6-web3-token-launchpad-main/1-token-launchpad-starter/src/components/TokenLaunchpad.jsx
+++ Token LaunchPad/week-6-web3-token-launchpad-main/1-token-launchpad-starter/src/components/TokenLaunchpad.jsx
@@ -1,5 +1,24 @@
-
+import {createMint, getMinimumBalanceForRentExemptMint} from "@solana/spl-token";
+import {Transaction, SystemProgram} from "@solana/web3.js";
 export function TokenLaunchpad() {
+
+    async function createToken(){
+        const lamports = await getMinimumBalanceForRentExemptMint(connection);
+        
+            const transaction = new Transaction().add(
+                SystemProgram.createAccount({
+                    fromPubkey: payer.publicKey,
+                    newAccountPubkey: keypair.publicKey,
+                    space: MINT_SIZE,
+                    lamports,
+                    programId,
+                }),
+                createInitializeMint2Instruction(keypair.publicKey, decimals, mintAuthority, freezeAuthority, programId),
+            );
+        
+            await sendAndConfirmTransaction(connection, transaction, [payer, keypair], confirmOptions);
+    }
+
     return  <div style={{
         height: '100vh',
         display: 'flex',
@@ -12,6 +31,6 @@ export function TokenLaunchpad() {
         <input className='inputText' type='text' placeholder='Symbol'></input> <br />
         <input className='inputText' type='text' placeholder='Image URL'></input> <br />
         <input className='inputText' type='text' placeholder='Initial Supply'></input> <br />
-        <button className='btn'>Create a token</button>
+        <button className='btn' onClick={createToken}>Create a token</button>
     </div>
 }
