748e0f78d41bdd1d1b091db82c661a52db68a0f9
Mohfazam <mohd.sarwar.code@gmail.com>
2025-07-14 01:07:14 +0530

    Get Balance

--- DAPP/src/Components/RequestAirdrop.tsx
+++ DAPP/src/Components/RequestAirdrop.tsx
@@ -22,7 +22,7 @@ export const RequestAirdrop = () => {
     return (
         <div className="flex flex-col">
                 <input onChange={(e) => setAmount((Number(e.target.value)))} className="border-8 rounded-2xl p-4 m-4" type="text" placeholder="Amount.." />
-            <button onClick={requestAirdrop} className="border-4 rounded-xl p-2 mb-4 bg-amber-300 hover:cursor-pointer">Request AirDrop</button>
+            <button onClick={requestAirdrop} className="border-4 rounded-xl font-bold p-2 mb-4 bg-amber-300 hover:cursor-pointer">Request AirDrop</button>
             <strong className="m-2 mb-2">{wallet.publicKey?.toBase58()}</strong>
             <strong className="m-2 mb-4 border-l-4 rounded-xl p-2">{amount}</strong>
         </div>
--- DAPP/src/Components/ShowBalance.tsx
+++ DAPP/src/Components/ShowBalance.tsx
@@ -1,7 +1,39 @@
+import { useConnection, useWallet } from "@solana/wallet-adapter-react"
+import { useState } from "react";
+
 export const ShowBalance = () => {
+
+    const [balance, setBalance] = useState(0);
+
+    const wallet  = useWallet();
+    const publicKey = wallet.publicKey;
+
+    const {connection} = useConnection(); 
+
+    const getUserBalance = async () => {
+
+        if(!publicKey) return;
+
+        try{
+            const bal = await connection.getBalance(publicKey);
+            const solBal = bal / 1_000_000_000;
+        setBalance(() => solBal)
+        alert("Balance Fetched Successfully");
+        } catch(e){
+            alert("Something went wrong: " + e);
+        }
+    }
+
     return(
         <div>
-            balance of the User
+            <div>
+                <button className="border-4 p-2 m-2 hover:cursor-pointer" onClick={getUserBalance}>Fetch Balance</button>
+            </div>
+
+            <h3>The Balance of the user is: 
+                <strong>{balance}</strong>
+            </h3>
+
         </div>
     )
 }
