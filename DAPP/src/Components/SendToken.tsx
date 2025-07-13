f5155777f78552d0137c04cca3ae433cf85d8f97
Mohfazam <mohd.sarwar.code@gmail.com>
2025-07-14 02:07:27 +0530

    Transaction UI

--- DAPP/src/Components/SendToken.tsx
+++ DAPP/src/Components/SendToken.tsx
@@ -25,18 +25,19 @@ export const SendToken = () => {
 
         await wallet.sendTransaction(transaction, connection);
 
-        alert("Set " + amount +  "SOL to " + sender);
+        alert("Set " + amount +  " SOL to " + sender);
 
     }
 
 
     return(
-        <div>
-           <input type="text" placeholder="To" onChange={(e) => setSender(e.target.value)}/>
+        <div className="flex flex-col">
+            <h1 className="text-center mb-4 text-4xl">Transfer <strong>SOL</strong> in <strong>DEVNET</strong></h1>
+           <input type="text" placeholder="To" onChange={(e) => setSender(e.target.value)} className="border-4 m-2 p-2 w-106 rounded-4xl border-r-4 border-t-3 border-t-blue-700 border-b-3 border-b-blue-700"/>
 
-           <input type="Text" placeholder="Amount" onChange={(e) => setAmount(Number(e.target.value))}/>
+           <input type="Text" placeholder="Amount" onChange={(e) => setAmount(Number(e.target.value))} className="border-4 m-2 p-2 w-106 rounded-4xl border-r-4 border-t-3 border-t-blue-700 border-b-3 border-b-blue-700"/>
 
-           <button className="p-2 m-2 w-22 rounded-lg bg-yellow-400 border-4 hover:cursor-pointer" onClick={sendToken}>Send</button>
+           <button className="p-2 m-2 w-106 font-bold rounded-lg bg-yellow-400 border-4 hover:cursor-pointer" onClick={sendToken}>Send</button>
         </div>
     )
 }
