6d0bd9d3de1eeadb84cacd93e19ce4e361ab9371
Mohfazam <mohd.sarwar.code@gmail.com>
2025-07-15 19:24:13 +0530

    Layout

--- DAPP/src/Components/Main/Layout.tsx
+++ DAPP/src/Components/Main/Layout.tsx
@@ -2,7 +2,7 @@ import { SolHEader } from "../SolHeader"
 
 export const Layout = () => {
     return(
-        <div className="min-h-screen bg-dark-primary">
+        <div className="min-h-screen bg-[#0a0a0a]">
             <div className="min-h-screen bg-dark-primary p-4">
                 <div className="max-w-6xl mx-auto">
                     <SolHEader />
