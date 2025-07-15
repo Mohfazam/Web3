9048ef54a80da2794aa8f167c470ac6c933922c9
Mohfazam <mohd.sarwar.code@gmail.com>
2025-07-15 20:23:45 +0530

    balance card

--- DAPP/src/Components/Card/BalanceCard.tsx
+++ DAPP/src/Components/Card/BalanceCard.tsx
@@ -1,9 +1,12 @@
+import { Wallet } from "lucide-react"
 import { CardTop } from "../UI/CardTop"
 
 export const BalanceCard = () => {
     return (
-        <div className="border border-white w-[680px]">
-            <CardTop />
+        <div className="bg-[#141414] border border-[#2a2a2a] rounded-xl p-6">
+            <div className="">
+                <CardTop Text="Balance" Icon={<Wallet size={24} />} />
+            </div>
         </div>
     )
 }
--- DAPP/src/Components/UI/CardTop.tsx
+++ DAPP/src/Components/UI/CardTop.tsx
@@ -1,12 +1,15 @@
-export const CardTop = () => {
-    return (
-        <div className="text-white">
-            <div>
-                Icon
-            </div>
-            <div>
-                Text
-            </div>
-        </div>
-    )
+import type{ ReactElement } from "react";
+
+interface CardTopProps {
+    Text: string;
+    Icon: ReactElement;
 }
+
+export const CardTop = ({ Text, Icon }: CardTopProps) => {
+    return (
+        <div className="text-white flex items-center gap-2 w-full h-full">
+            <div className="text-[#a3a3a3]">{Icon}</div>
+            <div className="text-lg font-medium text-[#ffffff]">{Text}</div>
+        </div>
+    );
+};
