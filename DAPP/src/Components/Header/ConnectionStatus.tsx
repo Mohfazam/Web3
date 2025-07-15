e23404b380f4ff6f2073a593a3e771760a5344e2
Mohfazam <mohd.sarwar.code@gmail.com>
2025-07-15 19:54:35 +0530

    Connection STatus Conditional

--- DAPP/src/Components/Header/ConnectionStatus.tsx
+++ DAPP/src/Components/Header/ConnectionStatus.tsx
@@ -1,13 +1,21 @@
 import { Wallet } from "lucide-react";
+import { useWallet } from "@solana/wallet-adapter-react";
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 
 export const ConnectionStatus = () => {
+
+    const { connected } = useWallet();
     return (
-        <div className="flex items-center gap-2 px-4 py-2 bg-[#141414] border border-[#2a2a2a] rounded-lg">
-            <div className="w-2 h-2 bg-[#22c55e] rounded-full" />
+        <div className="flex items-center gap-2 px-4 py-2 bg-[#141414] border border-[#2a2a2a] rounded-lg hover:cursor-pointer">
+            <div className={`w-2 h-2 rounded-full ${connected ? "bg-[#22c55e]" : "bg-red-500"}`} />
             <Wallet size={18} className="text-[#a3a3a3]" />
             <span className="text-sm text-[#a3a3a3] font-medium">
-                Connected
+                {connected ? "Connected" : "Connect"}
             </span>
+            <div>
+                <WalletMultiButton />
+            </div>
+             
         </div>
     );
 };
