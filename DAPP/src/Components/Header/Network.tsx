b2ef1e64736f70fd7bbc9cc89db015cd2eac2921
Mohfazam <mohd.sarwar.code@gmail.com>
2025-07-15 19:17:19 +0530

    DEVNET Final

--- DAPP/src/Components/Header/Network.tsx
+++ DAPP/src/Components/Header/Network.tsx
@@ -1,14 +1,10 @@
 export const Network = () => {
-    return(
-            <div className="border-[0.5px] rounded-lg border-[#4a3728] flex items-center w-[91px] h-[30px] pt-[6px] pb-[6px] pr-[12px] pl-[12px]">
-                <div className="w-2 h-2 bg-[#f59e0b] rounded-full mr-2">
-                     o
-                </div>
-            <div className="text-[#f59e0b]">
-                <span className="text-xs font-medium text-[#f59e0b]">
-                    DEVNET
-                </span>
-            </div>
+    return (
+        <div className="border border-[#4a3728] rounded-lg flex items-center w-[91px] h-[30px] px-3">
+            <div className="w-2 h-2 bg-[#f59e0b] rounded-full mr-2" />
+            <span className="text-xs font-medium text-[#f59e0b]">
+                DEVNET
+            </span>
         </div>
-    )
-}
+    );
+};
