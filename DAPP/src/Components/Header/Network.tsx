17b0dfdb2682a452dabc57ab49c41194612e648f
Mohfazam <mohd.sarwar.code@gmail.com>
2025-07-15 19:04:05 +0530

    DEVNET

---
+++ DAPP/src/Components/Header/Network.tsx
@@ -0,0 +1,12 @@
+export const Network = () => {
+    return(
+        <div className=" flex items-center w-[91px] h-[30px] pt-[6px] pb-[6px] pr-[12px] pl-[12px]">
+            <div className="w-2 h-2 bg-[#f59e0b] rounded-full mr-2">
+
+            </div>
+            <div className="bg-[#f59e0b]">
+                Devnet
+            </div>
+        </div>
+    )
+}
--- DAPP/src/Components/SolHeader.tsx
+++ DAPP/src/Components/SolHeader.tsx
@@ -1,16 +1,18 @@
+import { Network } from "./Header/Network"
+
 export const SolHEader = () => {
-    return(
+    return (
         <div className="flex items-center justify-between mb-8 bg-[#0a0a0a]">
             <h1 className="text-[#ffffff] text-2xl">
                 Solana Wallet
             </h1>
             <div className="flex items-center gap-3">
-                <div className="bg-red-700 w-24 h-14 border-blue-800 border-4 mr-16">
-                ejh
-            </div>
-            <div className="bg-red-700 w-24 h-14 mr-12">
-                sdkb
-            </div>
+                <div>
+                    <Network />
+                </div>
+                <div className="bg-red-700 w-24 h-14 mr-12">
+                    sdkb
+                </div>
             </div>
         </div>
     )
